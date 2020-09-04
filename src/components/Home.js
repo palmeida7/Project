cfca2bf447bbdfcc2b0c910e3f004e57f93bc36c
palmeida7 <pjalmeida7@gmail.com>
2020-09-04 07:49:22 -0400

    first changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ import IssueList from "./IssueList";
 import AddIssueForm from "./AddIssueForm";
 import Logout from "./Logout";
 import Email from "./Email";
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import './Home.css';
+
 
 
 function useIssues(){
@@ -28,15 +31,25 @@ function useIssues(){
   return issue
 
 }
+toast.configure()
 
 function Home() {
   const issues = useIssues()
 
+  const notify = () =>{
+    toast('Resolve Aged Priorities!')
+    toast.warn('Resolve High Priorities Before Noon!',{
+    autoClose:8000  
+    })
+  }
   return (
     <div className="App-home">
       <div class="home-div" >
         <div className="logout-div">
           <Logout />
+        </div>
+        <div className="toast-btn">
+          <button type="button" class="btn btn-warning" onClick={notify}>Reminder</button>
         </div>
         <div>
           <div className="proj-log"><h1>Project Restart</h1></div>
